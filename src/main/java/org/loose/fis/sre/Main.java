5c3019da955595c6f28144909eeddbaa15c929ad
CodrutaCraiovanu <72167484+CodrutaCraiovanu@users.noreply.github.com>
2022-05-22 02:09:13 +0300

    Update Main.java

--- src/main/java/org/loose/fis/sre/Main.java
+++ src/main/java/org/loose/fis/sre/Main.java
@@ -1,36 +1,46 @@
-package org.loose.fis.sre;
+package proiect.fis.C-C;
 
+import java.io.File;
+import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.loose.fis.sre.services.FileSystemService;
-import org.loose.fis.sre.services.UserService;
-
-import java.nio.file.Files;
-import java.nio.file.Path;
+import org.apache.commons.io.FileUtils;
 
 public class Main extends Application {
+    public Main() {
+    }
 
-    @Override
+    private static void copyFile(File src, File dest) throws IOException {
+        FileUtils.copyFileToDirectory(src, dest);
+    }
+
     public void start(Stage primaryStage) throws Exception {
-        initDirectory();
-        UserService.initDatabase();
-        Parent root = FXMLLoader.load(getClass().getClassLoader().getResource("register.fxml"));
-        primaryStage.setTitle("Registration Example");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = (Parent)FXMLLoader.load(this.getClass().getClassLoader().getResource("Login.fxml"));
+        primaryStage.setScene(new Scene(root, 600.0D, 520.0D));
         primaryStage.show();
     }
 
-    private void initDirectory() {
-        Path applicationHomePath = FileSystemService.APPLICATION_HOME_PATH;
-        if (!Files.exists(applicationHomePath))
-            applicationHomePath.toFile().mkdirs();
-    }
-
-
     public static void main(String[] args) {
+        File from_data = (new File("src\\main\\java\\data\\data.json")).getAbsoluteFile();
+        File from_city = (new File("src\\main\\java\\data\\city.json")).getAbsoluteFile();
+        File from_login = (new File("src\\main\\java\\data\\logininfo.json")).getAbsoluteFile();
+        File from_request = (new File("src\\main\\java\\data\\request.json")).getAbsoluteFile();
+        File from_rooms = (new File("src\\main\\java\\data\\rooms.json")).getAbsoluteFile();
+        File to = new File("target\\src\\main\\java\\data");
+
+        try {
+            copyFile(from_data, to);
+            copyFile(from_city, to);
+            copyFile(from_login, to);
+            copyFile(from_request, to);
+            copyFile(from_rooms, to);
+        } catch (IOException var8) {
+            var8.printStackTrace();
+        }
+
         launch(args);
     }
 }
